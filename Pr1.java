1ddfb98d2f623b1f5d002bec443d22368987f58b
sahay1503 <113369476+sahay1503@users.noreply.github.com>
2022-09-12 20:55:36 +0530

    Create Pr1.java

---
+++ Pr1.java
@@ -0,0 +1,17 @@
+//Take values of length and breadth of a rectangle from user and check if it is square or not.
+import java.util.*;
+public class Program1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        if(l==b){
+            System.out.println("It is a Square");
+        }else{
+            System.out.println("Not a Square");
+        }
+
+    }
+
+    
+}
